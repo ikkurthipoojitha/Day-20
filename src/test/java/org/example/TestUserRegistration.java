0bf1267a2984f9372c8c4d95c7d15d4586ec4bc3
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-06-26 15:57:53 +0530

    updated with sad test cases

--- src/test/java/org/example/TestUserRegistration.java
+++ src/test/java/org/example/TestUserRegistration.java
@@ -1,48 +1,91 @@
 package org.example;
 
-
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import  static org.junit.Assert.assert;
 
 
 public class TestUserRegistration {
     UserRegistration userRegistration = new UserRegistration();
 
-    @BeforeAll
+    @BeforeEach
     public void initialize(){
         userRegistration = new UserRegistration();
     }
 
-
     @Test
     public void givenFirstName_whenChecked_shouldReturnHappy(){
-        boolean result = userRegistration.validateFirstName("^[A-Z][a-z]{2,}$","Riya");
-        assert result;
+        boolean result = userRegistration.validateFirstName("^[A-Z][a-z]{2,}$","Teja");
+        Assertions.assertTrue(result);
     }
+
+    @Test
+    public void givenFirstName_whenChecked_shouldReturnSad(){
+        boolean result = userRegistration.validateFirstName("^[A-Z][a-z]{2,}$","abcd");
+        Assertions.assertFalse(result);
+    }
+
 
     @Test
     public void givenLastName_whenChecked_shouldReturnHappy(){
-        boolean result = userRegistration.validateFirstName("^[A-Z][a-z]{2,}$","Riya");
-        assert result;
+        boolean result = userRegistration.validateLastName("^[A-Z][a-z]{2,}$","Kavya");
+        Assertions.assertTrue(result);
     }
+
+    @Test
+    public void givenLastName_whenChecked_shouldReturnSad(){
+        boolean result = userRegistration.validateLastName("^[A-Z][a-z]{2,}$","Ab");
+        Assertions.assertFalse(result);
+    }
+
 
     @Test
     public void givenPassword_whenChecked_shouldReturnHappy(){
-        boolean result = userRegistration.validatePassword("^[A-Z][0-9][@#]{1}[a-z]{5,}$","K9@hgjkjijk");
-        assert result;
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        boolean result = userRegistration.validatePassword(regex,"Poojitha@123");
+        Assertions.assertTrue(result);
     }
 
     @Test
-    public void givenMobileNo_whenChecked_shouldReturnHappy(){
-        boolean result = userRegistration.validateMobileNo("^[9][1]\\s[9][0-9]{9}$","91 9876543892");
-        assert result;
+    public void givenPassword_whenChecked_shouldReturnSad(){
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        boolean result = userRegistration.validatePassword(regex,"bjknkl@123");
+        Assertions.assertFalse(result);
     }
+
+
+    @Test
+    public void givenMobileNumber_whenChecked_shouldReturnHappy(){
+        boolean result = userRegistration.validateMobileNumber("^[0-9]{1,2}\\s[6-9][0-9]{9}$","91 9876543210");
+        Assertions.assertTrue(result);
+
+    }
+    @Test
+    public void givenMobileNumber_whenChecked_shouldReturnSad(){
+        boolean result = userRegistration.validateMobileNumber("^[0-9]{1,2}\\s[6-9][0-9]{9}$","91 98543210");
+        Assertions.assertFalse(result);
+
+    }
+
 
     @Test
     public void givenEmail_whenChecked_shouldReturnHappy(){
-        boolean result = userRegistration.validateEmail("^[a-z]{3}[.][x-z]{3}[@][a-z]{2}[.][c]{1}[o]{1}[.][i][n]$", "abc.xyz@bl.co.in");
-        assert result;
+        boolean result = userRegistration.validateEmail("^[a-z]{3}[_][a-z0-9]{3}[@][a-z]{2}[.][c]{1}[o]{1}[.][a-z]{2,5}$", "abc_100@ad.co.com");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenEmail_whenChecked_shouldReturnSad(){
+        boolean result = userRegistration.validateEmail("^[a-z]{3}[_][a-z0-9]{3}[@][a-z]{2}[.][c]{1}[o]{1}[.][a-z]{2,5}$", "abc_@ad.co.com");
+        Assertions.assertFalse(result);
     }
 
 
